7bd70ac4988a6e83e4fa67aafaf476447c280595
Ricardo Maes <ricmzn@gmail.com>
2021-04-07 20:03:28 -0300

    Add logistics to strategic asset table

--- src/dct/enum.lua
+++ src/dct/enum.lua
@@ -101,6 +101,7 @@ enum.assetClass = {
 		[enum.assetType.AIRBASE]     = true,
 		[enum.assetType.SPECIALFORCES] = true,
 		[enum.assetType.FOB]           = true,
+		[enum.assetType.LOGISTICS]     = true,
 	},
 	-- agents never get seralized to the state file
 	["AGENTS"] = {
