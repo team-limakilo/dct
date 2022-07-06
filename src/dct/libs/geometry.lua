1e6372303e4f6d6f2835b91c3bead75cdd442d77
Jason du Plessis <jason.duplessis@ebtax.co.za>
2022-07-06 06:10:41 +0200

    Update meanCenter2D to Calculate accurate center for borders

--- src/dct/libs/geometry.lua
+++ src/dct/libs/geometry.lua
@@ -57,15 +57,51 @@ function Line:intersection(other)
 end
 
 function geometry.meanCenter2D(points)
-	local min = vec.Vector2D.create( math.huge,  math.huge)
-	local max = vec.Vector2D.create(-math.huge, -math.huge)
-	for _, point in ipairs(points) do
-		min.x = math.min(min.x, point.x)
-		min.y = math.min(min.y, point.y)
-		max.x = math.max(max.x, point.x)
-		max.y = math.max(max.y, point.y)
+	--Adapted from the following https://stackoverflow.com/a/2792459
+	local count = 0;
+	local center = vec.Vector2D.create(0, 0);
+	local signedArea = 0.0;
+	local x0 = 0.0;
+	local y0 = 0.0;
+	local x1 = 0.0;
+	local y1 = 0.0;
+	local a = 0;
+
+	--Get the count of the points
+	for _ in ipairs(points) do
+		count = count + 1;
 	end
-	return (max + min) / 2
+
+	for i = 1, count, 1 do
+		--Get the current x and y coordinates
+		x0 = points[i].x;
+		y0 = points[i].y;
+
+		--Get the next x and y coordinates, if it's the end of the array, get the first entries coordinates
+		if (i + 1) % count == 0 then
+			x1 = points[1].x;
+			y1 = points[1].y;
+		else
+			x1 = points[(i + 1) % count].x;
+			y1 = points[(i + 1) % count].y;
+		end
+
+		--calculate the partially sighned area
+		a = x0*y1 - x1*y0;
+		signedArea = signedArea + a;
+
+		--Update the Vector2D coordinates
+		center.x = center.x + (x0 + x1)*a;
+		center.y = center.y + (y0 + y1)*a;
+	end
+
+	--Calculate the centeriod
+	signedArea = signedArea * 0.5;
+	center.x = center.x / (6.0*signedArea);
+	center.y = center.y / (6.0*signedArea);
+
+	-- return the Vector2d
+	return center;
 end
 
 local function get(tbl, idx)
